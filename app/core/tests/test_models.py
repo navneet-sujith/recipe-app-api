de9b554e3fc15d22fd7c54622acdb0cfdfa95d46
navneet <navneetsujith@gmail.com>
2022-02-15 11:30:33 +0530

    Added TDD for Custom User Model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+
+class Modeltest(TestCase):
+    
+    def test_create_user_email_successful(self):
+        """Test creating a new user with email is successful"""
+        email = "test@somehing.dev"
+        password = "Testpass123"
+        user = get_user_model().objects.create_user(email=email, password=password)
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test whether the email for a new user is normalized"""
+        email = 'test@SOMETHING.DEV'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_invalid_user_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@something.dev',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+        
